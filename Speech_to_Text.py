64fa8bf0f8e7745532fe80a7c3c4b154898ef60e
Yuto Nakano <yuto.44.bb@gmail.com>
2018-12-14 22:48:41 +0900

    Update speech_text

--- Speech_to_Text.py
+++ Speech_to_Text.py
@@ -7,7 +7,7 @@ from ToneAnalyzer import analyze_emotion_from_text
 from TranslateText import translate_text
 import time
 import threading
-import Speech_Input
+from Speech_Input import timekeep, recoding
 from ToneAnalyzer import analyze_emotion_from_text
 
 
@@ -22,47 +22,54 @@ def speech_text():
     音声ファイルからテキストにする
     Watson - Text to Speech
     """
+    try:
+        # 音声入力したファイル
+        files = ['Speech.wav']
 
-    # 音声入力したファイル
-    files = ['Speech.wav']
+        for file in files:
+            with open(join(dirname(__file__), './.', file),
+                      'rb') as audio_file:
+                speech_recognition_results = speech_to_text.recognize(
+                    audio=audio_file,
+                    model='ja-JP_BroadbandModel',
+                    content_type='audio/wav',
+                    timestamps=True,
+                    word_alternatives_threshold=0.9,
+                    keywords=['colorado', 'tornado', 'tornadoes'],
+                    keywords_threshold=0.5
+                ).get_result()
+            p = json.dumps(speech_recognition_results,
+                           indent=2, ensure_ascii=False)
 
-    for file in files:
-        with open(join(dirname(__file__), './.', file),
-                  'rb') as audio_file:
-            speech_recognition_results = speech_to_text.recognize(
-                audio=audio_file,
-                model='ja-JP_BroadbandModel',
-                content_type='audio/wav',
-                timestamps=True,
-                word_alternatives_threshold=0.9,
-                keywords=['colorado', 'tornado', 'tornadoes'],
-                keywords_threshold=0.5
-            ).get_result()
-        p = json.dumps(speech_recognition_results,
-                       indent=2, ensure_ascii=False)
+            data = json.loads(p)
+        # 音声テキストのみ抽出
 
-        data = json.loads(p)
-    # 音声テキストのみ抽出
+        for i in data["results"]:
+            text = i["alternatives"][0]["transcript"]
 
-    for i in data["results"]:
-        text = i["alternatives"][0]["transcript"]
+        t = translate_text(text)
 
-    t = translate_text(text)
+        dic_English = json.loads(t)
 
-    dic_English = json.loads(t)
+        p = dic_English["translations"]
 
-    p = dic_English["translations"]
+        for i in p:
+            a = i['translation']
+        json_emotion = analyze_emotion_from_text(a)
+        emotion_dict = json.loads(json_emotion)
+        result = emotion_dict['document_tone']['tones'][0]['tone_name']
+        emotion_list.append(result)
+    except UnboundLocalError:
+        print("もっとはっきりしゃべってください！！")
+    except IndexError:
+        print("正しく感情が認識されませんでした・・・")
 
-    for i in p:
-        a = i['translation']
-    json_emotion = analyze_emotion_from_text(a)
-    emotion_dict = json.loads(json_emotion)
-    a = emotion_dict['document_tone']['tones'][0]['tone_name']
-    print(a)
 
+emotion_list = []
 
 # rangeに何回レコーディングをループさせるか
-funcs = [Speech_Input.timekeep, Speech_Input.recoding, speech_text]
+
+funcs = [timekeep, recoding, speech_text]
 threads = []
 current_time = time.time()
 
@@ -74,3 +81,10 @@ for i in range(1, 3):
 
     for t in threads:
         t.join()
+
+
+# [最終結果:感情を表す英単語の文字列]
+emotion_result = emotion_list[0]
+
+print(emotion_list)
+print(emotion_result)
